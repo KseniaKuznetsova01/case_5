1b6a44fd8950ff27aede3e908810e9a972e43eec
KseniaKuznetsova01 <ksenks.one@gmail.com>
2019-11-27 21:10:32 +0700

    part3

--- case_5.py
+++ case_5.py
@@ -5,24 +5,23 @@ Kuznetsova K.
 
 """
 import urllib.request
-url = 'http://www.nfl.com/player/brycepetty/2552369/profile'
-f = urllib.request.urlopen(url)
-s = f.read()
-text = str(s)
-part_name = text.find('player-name')
-name = text[text.find('>', part_name)+1:text.find('&', part_name)]
-part_total = text.find('TOTAL')
-total = text[text.find('>', part_total) + 1:text.find('</tr>', part_total)]
-new_total = ''
-i = total.find('</td>')
-m = total.find('<td>')
-while i != -1:
-    num = total[m + 4: i]
-    new_total += num + ' '
-    total = total[i + 1:]
-    i = total.find('</td>')
-    m = total.find('<td>')
-
-
-print(new_total)
-print(name)
+with open('input.txt') as inp_file:
+    for line in inp_file.readlines():
+        url = line
+        f = urllib.request.urlopen(url)
+        s = f.read()
+        text = str(s)
+        part_name = text.find('player-name')
+        name = text[text.find('>', part_name)+1:text.find('&', part_name)]
+        part_total = text.find('TOTAL')
+        total = text[text.find('>', part_total) + 1:text.find('</tr>', part_total)]
+        new_total = ''
+        i = total.find('</td>')
+        m = total.find('<td>')
+        while i != -1:
+            num = total[m + 4: i]
+            new_total += num + ' '
+            total = total[i + 1:]
+            i = total.find('</td>')
+            m = total.find('<td>')
+        print(name, new_total)
